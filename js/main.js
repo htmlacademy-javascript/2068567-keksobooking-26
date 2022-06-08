22b773dfe0d51b9b0cb2da9b72317b302904e449
Максим <maxim@gmail.com>
2022-06-08 18:35:49 +0400

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomInt (min, max) {
-  if (min >= max) {
-    return Math.floor(Math.random() * ((min - max) + 1)) + max;
+  if (min > max) {
+    return getRandomInt(max, min);
   }
   return Math.floor(Math.random() * ((max - min) + 1)) + min;
 }
@@ -14,12 +14,11 @@ getRandomInt(2,3);
 // Диапазон может быть только положительный, включая ноль
 
 function getRandomFloat (min, max, amount) {
-  if (min >= max) {
-    const swapRange = (Math.random() * (min - max)) + max;
-    return +(swapRange.toFixed(amount));
+  if (min > max) {
+    return getRandomFloat(max, min, amount);
   }
   const range = (Math.random() * (max - min)) + min;
-  return +(range.toFixed(amount));
+  return Number(range.toFixed(amount));
 }
 
 getRandomFloat(2,3,1);
