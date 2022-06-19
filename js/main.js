b886fbe3b55e77282d835c70f4935a7d91da173d
Максим <maxim@gmail.com>
2022-06-19 22:43:50 +0400

    Корректировка и добавление функций

--- js/main.js
+++ js/main.js
@@ -2,25 +2,23 @@
 // Диапазон может быть только положительный, включая ноль
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomInt (min, max) {
+const getRandomInt = (min, max) => {
   if (min > max) {
     return getRandomInt(max, min);
   }
   return Math.floor(Math.random() * ((max - min) + 1)) + min;
-}
+};
 
 // Возвращает случайное число с плавающей точкой из переданного диапазона включительно
 // Диапазон может быть только положительный, включая ноль
 
-function getRandomFloat (min, max, amount) {
+const getRandomFloat = (min, max, amount) => {
   if (min > max) {
     return getRandomFloat(max, min, amount);
   }
   const range = (Math.random() * (max - min)) + min;
   return Number(range.toFixed(amount));
-}
-
-const AVATARS_INDEX = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+};
 
 const OFFER_TITLES = ['Заголовок объявления'];
 
@@ -55,39 +53,44 @@ const SIMILAR_OFFERS_COUNT = 10;
 
 // Случайный элемент массива
 
-function getRandomArrayElement (array) {
-  return array[getRandomInt (0, array.length - 1)];
-}
+const getRandomArrayElement = (array) => array[getRandomInt (0, array.length - 1)];
 
-// Неповторяющийся элемент массива
+// Перемешанный массив случайной длины
 
-function getNoRepeatElement (array) {
-  for (let i = 0; i <= array.length - 1; i++) {
-    const element = array[i];
-    array.push(array[i]);
-    array.splice(i, 1);
-    return element;
+const getRandomLengthSlice = (array) => array.slice(0, getRandomInt(0, array.length));
+
+const shuffleArray = (array) => {
+  const results = [];
+  const elements = [...array];  // копия входного массива которую будем изменять
+  for (let i = 0; i < array.length; i++) {
+    const randomElementIndex = getRandomInt(0, elements.length - 1);
+    const [pluckedRandomElement] = elements.splice(randomElementIndex, 1); // splice возвращает массив из одного элемента, используем декструктуризацию
+    results.push(pluckedRandomElement);
   }
-}
+  return results;
+};
 
-// Массив случайной длины без повторения значений
+const getRandomSlice = (array) => {
+  const shuffledElements = shuffleArray(array);
 
-function getRandomLength (array) {
-  for (let i = 0; i <= array.length - 1; i++) {
-    array.push(array[i]);
-    array.splice(i, 1);
-  }
-  const radnomLength = array.slice(getRandomInt(0, array.length - 1));
-  return radnomLength;
-}
+  return getRandomLengthSlice(shuffledElements);
+};
+
+//Число с ведущим нулем
+
+const fixedLengthString = (str, filler, length) => {
+  const chars = String(str).split('');
+  const template = Array.from({length}, () => filler);
+
+  return [...template, ...chars].join('').slice(-length);
+};
 
 //Генерация офера
 
-function createOffer () {
-  return { author: {
-    avatar: `img/avatars/user${getNoRepeatElement(AVATARS_INDEX)}.png`
+const createOffer = (_, index) => ({
+  author: {
+    avatar: `img/avatars/user${fixedLengthString(index + 1, '0', 2)}.png`
   },
-
   offer: {
     title: getRandomArrayElement(OFFER_TITLES),
     address: `${getRandomFloat (35.65, 35.7, 5)}, ${getRandomFloat (139.7, 139.8, 5)}`,
@@ -97,17 +100,15 @@ function createOffer () {
     guests: getRandomInt (1, 3),
     checkin: getRandomArrayElement(TIME),
     checkout: getRandomArrayElement(TIME),
-    features: getRandomLength(FEATURES),
+    features: getRandomSlice(FEATURES),
     description: getRandomArrayElement(OFFER_DESCRIPTIONS),
-    photos: getRandomLength(PHOTOS),
+    photos: getRandomLengthSlice(PHOTOS),
   },
 
   location: {
     lat: getRandomFloat (35.65, 35.7, 5),
     lng: getRandomFloat (139.7, 139.8, 5)
   }
-  };
-}
+});
 
 const similarOffers = Array.from({length: SIMILAR_OFFERS_COUNT}, createOffer);
-
